fad84eb3e48f34d179b05dad013fb243f8538cf5
Eduardodac <eduardo.dac@outlook.com>
2023-05-28 16:54:11 -0600

    Agrega controlador de usuarios

---
+++ SistemaMonitoreoAlimentacionApi/Controllers/UsuariosController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaMonitoreoAlimentacionApi.Entidades;
+
+namespace SistemaMonitoreoAlimentacionApi.Controllers
+{
+    [ApiController]
+    [Route("api/usuarios")]
+    public class UsuariosController: ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UsuariosController (ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Usuarios>> GetUsuarios()
+        {
+            return new List<Usuarios>();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PostUsuario(Usuarios usuario)
+        {
+            _context.Add(usuario);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
