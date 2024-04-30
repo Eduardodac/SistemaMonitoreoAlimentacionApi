5cc5d8d8d872b5e5f376994facda64c101bee09d
Eduardodac <eduardo.dac@outlook.com>
2024-04-30 00:04:08 -0600

    Agrega metodo get y post en eactividad felina controller

---
+++ SistemaMonitoreoAlimentacionApi/Controllers/ActividadFelinaController.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SistemaMonitoreoAlimentacionApi.Dtos.ActividadFelina;
+using SistemaMonitoreoAlimentacionApi.Entidades;
+
+namespace SistemaMonitoreoAlimentacionApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ActividadFelinaController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        private readonly IMapper mapper;
+
+        public ActividadFelinaController(ApplicationDbContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        #region Get
+        [HttpGet("{gatoId}")]
+        public async Task<ActionResult<List<ActividadFelina>>> ListaActividadesFelinas([FromRoute] Guid gatoId)
+        {
+            var gatoAsignado = await context.Gatos.FirstOrDefaultAsync(g => g.GatoId.Equals(gatoId));
+
+            if (gatoAsignado == null)
+            {
+                return NotFound($"Este gato con id {gatoId} no existe");
+            }
+
+            return await context.ActividadesFelinas.Where(ac => ac.GatoId.Equals(gatoId)).ToListAsync();
+        }
+        #endregion
+        #region Post
+        [HttpPost]
+        public async Task<ActionResult> CrearActividadFelina([FromBody] NuevaActividadFelinaDto nuevaActividadFelinaDto) 
+        {
+            var gatoAsignado = await context.Gatos.FirstOrDefaultAsync(g => g.GatoId.Equals(nuevaActividadFelinaDto.GatoId));
+
+            if (gatoAsignado == null)
+            {
+                return NotFound($"Este gato con id {nuevaActividadFelinaDto.GatoId} no existe");
+            }
+
+            var gato = mapper.Map<ActividadFelina>(nuevaActividadFelinaDto);
+
+            context.Add(gato);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+        #endregion
+    }
+}
---
+++ SistemaMonitoreoAlimentacionApi/Dtos/ActividadFelina/NuevaActividadFelinaDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaMonitoreoAlimentacionApi.Dtos.ActividadFelina
+{
+    public class NuevaActividadFelinaDto
+    {
+        public Guid ActividadFelinaId { get; set; } = Guid.Empty!;
+        [Required]
+        public Guid GatoId { get; set; } = Guid.Empty!;
+        [Required]
+        public DateTime FechaInicio { get; set; } = DateTime.MinValue!;
+        [Required]
+        public DateTime FechaFin { get; set; } = DateTime.MinValue!;
+        public int Aproximaciones { get; set; } = 0;
+        public double AlimentoConsumido { get; set; } = 0.0;
+        public int AproximacionesSinConsumo { get; set; } = 0;
+    }
+}
