50cee23260ee9ee58e220875c7426b0dc0b178ec
Eduardodac <eduardo.dac@outlook.com>
2024-05-04 23:13:21 -0600

    Agrega validaciones de entidad horario

--- SistemaMonitoreoAlimentacionApi/Entidades/Horario.cs
+++ SistemaMonitoreoAlimentacionApi/Entidades/Horario.cs
@@ -5,9 +5,11 @@ namespace SistemaMonitoreoAlimentacionApi.Entidades
     public class Horario
     {
         public Guid HorarioId { get; set; } = Guid.Empty!;
+        [Required(ErrorMessage = "El campo {0} es requerido")]
         public int DiaDeLaSemanaId { get; set; } = 0;
-        [Required]
+        [Required(ErrorMessage = "El campo {0} es requerido")]
         public Guid UsuarioId { get; set; } = Guid.Empty!;
+        [Required(ErrorMessage = "El campo {0} es requerido")]
         public DateTime Hora { get; set; } = DateTime.Now;
         public Usuario? Usuario { get; set;}
         public DiadelaSemana? DiadelaSemana { get; set; }
