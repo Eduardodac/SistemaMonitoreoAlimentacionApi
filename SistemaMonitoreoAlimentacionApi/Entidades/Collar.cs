49ed8384ca7f57c0e1809eb9b0ce3aedb82d6862
Eduardodac <eduardo.dac@outlook.com>
2024-05-04 23:18:42 -0600

    Agregar validaciones de collares

--- SistemaMonitoreoAlimentacionApi/Entidades/Collar.cs
+++ SistemaMonitoreoAlimentacionApi/Entidades/Collar.cs
@@ -5,12 +5,12 @@ namespace SistemaMonitoreoAlimentacionApi.Entidades
     public class Collar
     {
         public Guid CollarId { get; set; } = Guid.Empty!;
-        [Required]
-        public DateTime? FechaSalida { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        public DateTime FechaSalida { get; set; } = DateTime.Now;
         public DateTime? FechaActivacion{ get; set; }
-        [Required]
+        [Required(ErrorMessage = "El campo {0} es requerido")]
         public string NumeroRegistro { get; set; } = null!;
-        [Required]
+        [Required(ErrorMessage = "El campo {0} es requerido")]
         public Boolean EstatusActivacion { get; set; } = false;
         public Gato? Gato { get; set; }
     }
